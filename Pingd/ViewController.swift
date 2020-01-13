ef9925a262bc2dede08db0d087bff523a28ba08f
David Acevedo <dacevedo.cs@gmail.com>
2020-01-12 20:48:45 -0500

    Initial Commit

---
+++ Pingd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pingd
+//
+//  Created by David Acevedo on 1/12/20.
+//  Copyright © 2020 David Acevedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
