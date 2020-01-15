44b919626d3f805026657a05e20cbc33e02c25db
David Acevedo <dacevedo.cs@gmail.com>
2020-01-15 00:37:18 -0500

    Initial Commit

---
+++ Pingd/OnboardingView.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingView.swift
+//  Pingd
+//
+//  Created by David Acevedo on 1/12/20.
+//  Copyright © 2020 David Acevedo. All rights reserved.
+//
+
+import UIKit
+import paper_onboarding
+
+class OnboardingView: PaperOnboarding {
+
+    
+
+}
---
+++ Pods/Target Support Files/Pods-Pingd/Pods-Pingd-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pingd : NSObject
+@end
+@implementation PodsDummy_Pods_Pingd
+@end
